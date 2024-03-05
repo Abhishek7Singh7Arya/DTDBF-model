66969cd6c3c6412c3a62b9e8bce9355c3b1cbf40
Abhishek7Singh7Arya <abhisheksingharya77@gmail.com>
2024-03-05 17:53:37 +0530

    initial commit

---
+++ bench/bench.cpp
@@ -0,0 +1,259 @@
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <random>
+
+#include "../include/proteus.hpp"
+#include "../include/modeling.hpp"
+#include "../include/util.hpp"
+#include "../SuRF/include/surf.hpp"
+
+#define SAVE_TO_RESCSV(val) rescsv << val << ",";
+
+static size_t surf_hlen = 0, surf_rlen = 0;
+static double sample_proportion = 0.0;
+static bool is_int_bench;
+static size_t keylen;
+static double bpk;
+static bool use_Proteus;
+static bool use_SuRF;
+
+
+std::ofstream rescsv;
+
+
+inline bool isPointQuery(const uint64_t& a, const uint64_t& b) {
+    return b == (a + 1);
+}
+
+inline bool isPointQuery(const std::string& a, const std::string& b) {
+    return b == a;
+}
+
+template<typename T>
+void runExperiment(std::vector<T>& keys,
+                   std::set<T>& keyset,
+                   std::vector<std::pair<T, T>>& queries,
+                   std::vector<std::pair<T, T>>& sample_queries,
+                   std::vector<std::string>& skeys,
+                   std::vector<std::pair<std::string, std::string>>& squeries) {
+    
+    clock_t begin, end;
+    begin = end = clock();
+    surf::SuRF* surf = 0;
+    proteus::Proteus* proteus = 0;
+
+    if (use_Proteus) {
+        begin = clock();
+       
+    // proteus::Proteus proteusInstance; // Create a Proteus instance
+    // uint64_t t = proteusInstance.processKey<T>(keys[0], 6); // Call processKey using the instance
+    // std::cout << "original trie " << keys[0] << " edited trie " << t << std::endl;
+    
+     std::tuple<size_t, size_t, size_t> parameters = proteus::modeling(keys, sample_queries, bpk, keylen);
+        end = clock();
+        unsigned long modeling_time = 1000*(end-begin)/CLOCKS_PER_SEC;
+        SAVE_TO_RESCSV(modeling_time)
+
+        printf("Modeling Time/ms: %lu\n", modeling_time);
+        printf("Trie Depth: %zu; Sparse-Dense Cutoff (bytes): %zu; BF Prefix Length: %zu\n", 
+               std::get<0>(parameters), std::get<1>(parameters), std::get<2>(parameters));
+        SAVE_TO_RESCSV(std::get<0>(parameters))
+        SAVE_TO_RESCSV(std::get<2>(parameters))
+
+        begin = clock();
+        proteus = new proteus::Proteus(keys, 
+                                       std::get<0>(parameters), // Trie Depth
+                                       std::get<1>(parameters), // Sparse-Dense Cutoff
+                                       std::get<2>(parameters), // Bloom Filter Prefix Length
+                                       bpk); // BPK
+        end = clock();
+
+    } else if (use_SuRF) {
+        begin = clock();
+        if (surf_hlen == 0 && surf_rlen == 0) {
+            surf = new surf::SuRF(skeys, true, 64, surf::kNone, surf_hlen, surf_rlen);
+        } else if (surf_rlen == 0) {
+            surf = new surf::SuRF(skeys, true, 64, surf::kHash, surf_hlen, surf_rlen);
+        } else if (surf_hlen == 0) {
+            surf = new surf::SuRF(skeys, true, 64, surf::kReal, surf_hlen, surf_rlen);
+        } else {
+            surf = new surf::SuRF(skeys, true, 64, surf::kMixed, surf_hlen, surf_rlen);
+        }
+        end = clock();
+    } 
+
+    printf("\tus/Insert:\t%lf\n", (double)1000000*(end-begin)/CLOCKS_PER_SEC/keys.size());
+
+    // Test serialization
+    if (use_Proteus) {
+        auto ser = proteus->serialize();
+        delete proteus;
+        uint8_t* ser_tofree = ser.first;
+        proteus = proteus::Proteus::deSerialize((char*) ser.first);
+        delete[] ser_tofree;
+    }
+
+    begin = clock();
+    size_t empty = 0, fp = 0, fn = 0;
+    for (size_t i = 0; i < queries.size(); i++) {
+        bool full = false;
+        bool filterAns = true;
+        
+        if (use_Proteus) {
+            if (isPointQuery(queries[i].first, queries[i].second)) {
+                filterAns = proteus->Query(queries[i].first);
+            } else {
+                filterAns = proteus->Query(queries[i].first, queries[i].second);
+            }
+        } 
+        else if (use_SuRF) {
+            if (isPointQuery(queries[i].first, queries[i].second)) {
+                filterAns = surf->lookupKey(squeries[i].first);
+            } else {
+                filterAns = is_int_bench ? surf->lookupRange(squeries[i].first, true, squeries[i].second, false)
+                                         : surf->lookupRange(squeries[i].first, true, squeries[i].second, true);
+            }
+        }
+
+        auto it = keyset.lower_bound(queries[i].first);
+        full = it != keyset.end() &&
+               ((is_int_bench && (*it) < queries[i].second)||
+                (!is_int_bench && (*it) <= queries[i].second));
+
+        if (!full) {
+            ++empty;
+            if (filterAns) {
+                ++fp;
+            }
+        }
+        if (full && !filterAns) {
+            printf("False negative!\n");
+            fn++;
+        }
+    }
+    end = clock();
+    printf("\ttotal us/Query:\t%lf\n", (double)1000000*(end-begin)/CLOCKS_PER_SEC/queries.size());
+
+
+    bool res;
+    size_t sum;
+    begin = clock();
+    if (use_Proteus) {
+        for (size_t i = 0; i < queries.size(); i++) {
+            if (isPointQuery(queries[i].first, queries[i].second)) {
+                res = proteus->Query(queries[i].first);
+            } else {
+                res = proteus->Query(queries[i].first, queries[i].second);
+            }
+            sum += 1 ? res : 0;
+        }
+    } else if (use_SuRF) {
+        for (size_t i = 0; i < squeries.size(); i++) {
+            if (isPointQuery(queries[i].first, queries[i].second)) {
+                res = surf->lookupKey(squeries[i].first);
+            } else {
+                res = is_int_bench ? surf->lookupRange(squeries[i].first, true, squeries[i].second, false)
+                                   : surf->lookupRange(squeries[i].first, true, squeries[i].second, true);
+            }
+            sum += 1 ? res : 0;
+        }
+    }
+    end = clock();
+    printf("\tus/Query:\t%lf\n", (double)1000000*(end-begin)/CLOCKS_PER_SEC/queries.size());
+    double fpr = (double)fp/empty;
+    printf("\tFPR:\t%lf\n",fpr );
+    SAVE_TO_RESCSV(fpr)
+
+    rescsv << std::endl;
+    printf("empty: %zu, fn: %lu, fp: %lu\n", empty, fn, fp);
+
+    if (use_Proteus) {
+        auto ser = proteus->serialize();
+        printf("\tBPK:\t%lf\n", (double)ser.second*8/keys.size());
+        delete[] ser.first;
+    } else if (use_SuRF) {
+        printf("\tBPK:\t%lf\n", (double)surf->serializedSize()*8/keys.size());
+    } 
+    
+    if (use_Proteus) {
+        delete proteus;
+    } else if (use_SuRF) {
+        delete surf;
+    }
+
+ 
+    rescsv.close();
+}
+
+
+int main(int argc, char **argv) {
+    assert(argc == 6);
+
+    is_int_bench = strtoull(argv[1], nullptr, 10) == 1;
+    use_Proteus = (strcmp(argv[2], "Proteus") == 0);
+    use_SuRF = (strcmp(argv[2], "SuRF") == 0);
+
+    if (use_Proteus) {
+        
+
+        bpk = strtod(argv[3], nullptr);
+        sample_proportion = strtod(argv[4], nullptr);
+    } else if (use_SuRF) {
+        surf_hlen = strtoull(argv[3], nullptr, 10);
+        surf_rlen = strtoull(argv[4], nullptr, 10);
+    }
+   
+    rescsv.open(argv[5],std::ios_base::app);
+    
+    // Check if the file is opened successfully
+    if (!rescsv.is_open()) {
+        std::cerr << "Error opening file: " << argv[5] << std::endl;
+        return 1;
+    }
+
+    std::string dataPath = "./my_data/";
+    std::string keyFilePath = dataPath + "data0.txt";
+    std::string lQueryFilePath = dataPath + "txn0.txt";
+    std::string uQueryFilePath = dataPath + "upper_bound0.txt";
+
+    if (is_int_bench) {
+        keylen = 64;
+
+        std::vector<uint64_t> keys;
+        std::vector<std::string> skeys;
+        std::set<uint64_t> keyset;
+        std::vector<std::pair<uint64_t, uint64_t>> queries;
+        std::vector<std::pair<std::string, std::string>> squeries;
+        std::vector<std::pair<uint64_t, uint64_t>> sample_queries;
+
+        proteus::intLoadKeys(keyFilePath, keys, skeys, keyset);
+        proteus::intLoadQueries(lQueryFilePath, uQueryFilePath, queries, squeries);
+        
+        if (use_Proteus) {
+            sample_queries = proteus::sampleQueries(queries, sample_proportion);
+        }
+
+        runExperiment(keys, keyset, queries, sample_queries, skeys, squeries);
+
+    } else {
+        std::vector<std::string> keys;
+        std::set<std::string> keyset;
+        std::vector<std::pair<std::string, std::string>> queries;
+        std::vector<std::pair<std::string, std::string>> sample_queries;
+
+        keylen = proteus::strLoadKeys(keyFilePath, keys, keyset) * 8;
+        proteus::strLoadQueries(lQueryFilePath, uQueryFilePath, queries);
+        
+        if (use_Proteus) {
+            sample_queries = proteus::sampleQueries(queries, sample_proportion);
+        }
+
+        runExperiment(keys, keyset, queries, sample_queries, keys, queries);
+    }
+}
---
+++ bench/in-memory.sh
@@ -0,0 +1,291 @@
+#!/bin/bash
+
+# Reference: https://sharats.me/posts/shell-script-best-practices/
+
+# Exit if a command fails  
+set -o errexit
+
+# Throw error when accessing an unset variable
+set -o nounset
+
+# Enable debug mode with TRACE=1 ./bench.sh
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+# Change to directory of this script
+cd "$(dirname "$0")"
+
+###############################################
+###########   WORKLOAD PARAMETERS   ###########
+###############################################
+
+## NOTE: LINKED PARAMETERS
+#  By default, we loop through all combinations of parameters, but linked parameters 
+#  are considered as a single parameter.  i.e. 
+#
+#       kdist_arr=("kuniform" "kuniform")
+#       qdist_arr=("quniform" "qcorrelated")
+#
+#  Corresponds to two workloads with uniform key distributions, one with 
+#  uniform queries and the other with correlated queries.
+
+
+## 1. Key Length (in bits)
+#######################################
+# For integer workloads, this value MUST be 64
+# For synthetic string workloads, this value MUST be a multiple of 8
+# For the string domain workload, this value is ignored.
+
+
+## 2. Number of Keys
+## 3. Number of Queries
+#######################################
+
+
+## [LINKED]
+## 4. Key Distribution
+## 5. Query Distribution
+#######################################
+# For SOSD / Domain workloads, please download the relevant datasets and follow the instructions in the README
+# SOSD and Domain Key and Query Distributions must be specified together, e.g. ksosd_books + qsosd_books
+# Integer Key Distributions: kuniform, knormal, ksosd_books, ksosd_fb
+# Integer Query Distributions: quniform, qnormal, qcorrelated, qsplit, qsosd_books, qsosd_fb
+# String Key Distributions: kuniform, knormal, kdomain
+# String Query Distributions: quniform, qcorrelated, qsplit, qdomain
+
+
+## [LINKED]
+## 6. Minimum Range Size
+## 7. Maximum Range Size
+## 8. Point / Range Query Ratio
+#######################################
+# Min Range Size must be >= 2
+# Point / Range Query Ratio must be >= 0.0 && <= 1.0
+# Use 0.0 for all range queries and 1.0 for all point queries
+# For qsplit and pqratio <= 0.5, all point queries are assigned to the correlated distribution.
+# For qsplit and pqratio > 0.5, half the workload comprises correlated point queries and the 
+# remaining point queries are assigned to the uniform distribution.
+
+
+## 9. Positive / Negative Query Ratio
+#######################################
+# Must be >= 0.0 && <= 1.0
+# Use 0.0 for all negative queries and 1.0 for all positive queries
+
+
+## 10. Query Correlation Degree
+#######################################
+# The distance between the left query bound and a key is uniformly distributed between 1 and the specified value
+# Must be >= 1
+
+
+### INTEGER
+
+IS_INT_BENCH="1"
+keylen_arr=(64)
+nkeys_arr=(10000000)
+nqrys_arr=(1000000)
+kdist_arr=("ksosd_fb" "ksosd_books" "kuniform" "kuniform" "knormal" "knormal" "knormal")
+qdist_arr=("qsosd_fb" "qsosd_books" "quniform" "qcorrelated" "qsplit" "qcorrelated" "quniform")
+minrange_arr=(2 2 2 2) 
+maxrange_arr=(2 32 4096 1024) 
+pqratio_arr=(1.0 0.0 0.0 0.5)
+pnratio_arr=(0.0)
+corrd_arr=(1024)
+
+### STRING
+
+# IS_INT_BENCH="0"
+# keylen_arr=(1440)
+# nkeys_arr=(10000000)
+# nqrys_arr=(1000000)
+# kdist_arr=("kuniform" "knormal" "kuniform" "kdomain")
+# qdist_arr=("quniform" "qcorrelated" "qcorrelated" "qdomain")
+# minrange_arr=(2) 
+# maxrange_arr=(1073741824) 
+# pqratio_arr=(0.0)
+# pnratio_arr=(0.0)
+# corrd_arr=(536870912)
+
+
+if [[ "${IS_INT_BENCH}" == "1" ]]; then
+    echo -e "Running Integer Benchmark"
+else
+    echo -e "Running String Benchmark"
+fi
+
+
+###############################################
+############   FILTER PARAMETERS   ############
+###############################################
+
+
+### Proteus
+
+## Filter Bits-per-Key
+#######################################
+# Must be a positive real number
+membudg_arr=(8 10 12 14 16 18 20)
+
+## Sample Proportion
+#######################################
+# Determines the proportion of queries given to Proteus as a sample
+# Must be >= 0.0 && <= 1.0
+# Default sample size for in-memory benchmarks is 20K, see paper for sample size justification
+sampleprop_arr=(0.02)
+
+
+### SuRF
+
+## Hash and Real Suffix Lengths
+#####################################
+# These values must be non-negative integers and they control SuRF's memory usage.
+# Hash suffixes are only useful for point queries 
+surfhlen_arr=(0)
+surfrlen_arr=(0 2 4 6 8 10)
+
+
+################################################################
+
+REPO_DIR="$(pwd)"
+SOSD_DIR="$REPO_DIR/../workloads/SOSD/"
+DOMAINS="$REPO_DIR/../workloads/domains/domains.txt"
+DOMAINS_SHUFFLED="$REPO_DIR/../workloads/domains/shuf.txt"
+INT_WORKL_BIN="$REPO_DIR/../workloads/int_workload_gen"
+STR_WORKL_BIN="$REPO_DIR/../workloads/str_workload_gen"
+EXP_BIN="$REPO_DIR/bench"
+
+res_folder="$REPO_DIR/$1"
+if [ ! -e "$res_folder" ]; then 
+    mkdir "$res_folder"
+else
+    echo "Result folder already exists! Please try again with a valid result folder name."
+    exit -1
+fi
+#create result csv
+# Create result csv
+res_csv="$res_folder/results.csv"
+touch $res_csv
+
+
+experiment() {
+    cd "$expdir"
+    touch ./experiment_result
+    if [[ "${IS_INT_BENCH}" == "1" ]]; then
+        echo -e "### BEGIN INTEGER EXPERIMENT ###" >> ./experiment_result
+    else
+        echo -e "### BEGIN STRING EXPERIMENT ###" >> ./experiment_result
+    fi
+    echo -e "### BEGIN EXPERIMENT DESCRIPTION ###" >> ./experiment_result
+    echo -e "\tFilter Name:\t$filter" >> ./experiment_result
+    echo -e "\tNumber of Keys:\t$nkeys" >> ./experiment_result
+    echo -e "\tKey Length:\t$keylen" >> ./experiment_result
+    echo -e "\tNumber of Queries:\t$nqrys" >> ./experiment_result
+    echo -e "\tKey Distribution:\t$kdist" >> ./experiment_result
+    echo -e "\tQuery Distribution:\t$qdist" >> ./experiment_result
+    echo -e "\tMinimum Range Size:\t$minrange" >> ./experiment_result
+    echo -e "\tMaximum Range Size:\t$maxrange" >> ./experiment_result
+    echo -e "\tPoint / Range Query Ratio:\t$pqratio" >> ./experiment_result
+    echo -e "\tQuery Correlation Degree:\t$corrd" >> ./experiment_result
+    echo -e "\tPositive / Negative Query ratio:\t$pnratio" >> ./experiment_result
+    
+    if [ $IS_INT_BENCH = "1" ]; then
+        echo "$filter" "$INT_WORKL_BIN" "$SOSD_DIR" "$nkeys" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
+        $INT_WORKL_BIN "$SOSD_DIR" "$nkeys" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
+    else
+        # Shuffle domains on every run
+        if [ $kdist = "kdomain" ]; then
+            cat $DOMAINS | shuf -n $(($nkeys + $nqrys)) -o $DOMAINS_SHUFFLED
+        fi
+        echo "$filter" "$STR_WORKL_BIN" "$DOMAINS_SHUFFLED" "$nkeys" "$keylen" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
+        $STR_WORKL_BIN "$DOMAINS_SHUFFLED" "$nkeys" "$keylen" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
+    fi
+    
+    if [ $filter = "SuRF" ]; then
+        echo -e "\tSuRF Hash Suffix Length:\t$surfhlen" >> ./experiment_result
+        echo -e "\tSuRF Real Suffix Length:\t$surfrlen" >> ./experiment_result
+        echo -e "### END EXPERIMENT DESCRIPTION ###\n\n" >> ./experiment_result
+        $EXP_BIN "$IS_INT_BENCH" "$filter" "$surfhlen" "$surfrlen" "$res_csv" >> ./experiment_result
+    
+    elif [ $filter = "Proteus" ]; then
+        echo -e "\tFilter Bits-per-Key:\t$membudg" >> ./experiment_result
+        echo -e "\tSample Proportion:\t$sampleprop" >> ./experiment_result
+        echo -e "### END EXPERIMENT DESCRIPTION ###\n\n" >> ./experiment_result
+
+        echo -ne "$IS_INT_BENCH,$filter,$nkeys,$nqrys,$keylen,$kdist,$qdist,$minrange,$maxrange,$pqratio,$corrd,$pnratio,$membudg," >> $res_csv
+   
+        $EXP_BIN "$IS_INT_BENCH" "$filter" "$membudg" "$sampleprop" "$res_csv" >> ./experiment_result
+    fi
+
+    echo -e "### END EXPERIMENT ###" >> ./experiment_result
+    cp ./experiment_result "$res_folder/logdata.txt"
+
+    rm -rf ./my_data/
+    cat ./experiment_result
+    echo -e ""
+    rm experiment_result
+}
+
+# Proteus Experiments
+filter="Proteus"
+for nkeys in "${nkeys_arr[@]}"; do
+for keylen in "${keylen_arr[@]}"; do   
+for nqrys in "${nqrys_arr[@]}"; do
+for i in "${!kdist_arr[@]}"; do
+for j in "${!minrange_arr[@]}"; do
+for membudg in "${membudg_arr[@]}"; do
+for corrd in "${corrd_arr[@]}"; do
+for pnratio in "${pnratio_arr[@]}"; do
+for sampleprop in "${sampleprop_arr[@]}"; do
+
+kdist="${kdist_arr[$i]}"
+qdist="${qdist_arr[$i]}"
+minrange="${minrange_arr[$j]}"
+maxrange="${maxrange_arr[$j]}"
+pqratio="${pqratio_arr[$j]}"
+
+expdir=$(mktemp -d /tmp/proteus_exp.XXXXXXX)
+experiment
+
+done
+done
+done
+done
+done
+done
+done
+done
+done
+
+
+# SuRF experiments
+filter="SuRF"
+for nkeys in "${nkeys_arr[@]}"; do
+for keylen in "${keylen_arr[@]}"; do
+for nqrys in "${nqrys_arr[@]}"; do
+for i in "${!kdist_arr[@]}"; do
+for j in "${!minrange_arr[@]}"; do
+for corrd in "${corrd_arr[@]}"; do
+for pnratio in "${pnratio_arr[@]}"; do
+for surfhlen in "${surfhlen_arr[@]}"; do
+for surfrlen in "${surfrlen_arr[@]}"; do
+
+kdist="${kdist_arr[$i]}"
+qdist="${qdist_arr[$i]}"
+minrange="${minrange_arr[$j]}"
+maxrange="${maxrange_arr[$j]}"
+pqratio="${pqratio_arr[$j]}"
+
+expdir=$(mktemp -d /tmp/surf_exp.XXXXXXX)
+experiment
+
+done
+done
+done
+done
+done
+done
+done
+done
+done
